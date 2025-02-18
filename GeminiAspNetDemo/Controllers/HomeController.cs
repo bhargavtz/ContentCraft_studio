c9f3b1500476ce95471f2cb272095bc85ce80ec4
bhargavtz <bhargav05@yandex.com>
2025-02-18 20:09:48 +0530

    adding page Content,About,Privacy
    1. Every page has the same navigation menu with Home, About, Contact, and Privacy links
    The footer on each page includes links to Privacy Policy
    The Contact form includes a privacy notice with a link to the Privacy Policy
    The Privacy Policy page includes a link to the Contact page
    All links use proper routing with /Home/[Action] format
    You can navigate between pages using:
    The top navigation menu
    Footer links
    In-content links (like the privacy notice in the contact form)
    The links in the Privacy Policy page

--- GeminiAspNetDemo/Controllers/HomeController.cs
+++ GeminiAspNetDemo/Controllers/HomeController.cs
@@ -21,6 +21,16 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
     public IActionResult Chat()
     {
         return View();
